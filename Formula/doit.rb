bcc88f2df5c7a76b66895225ed58628b8641fbad
William Crump <rickycrump95@gmail.com>
2018-10-31 14:09:22 -0400

    Removed random sha value.

--- Formula/doit.rb
+++ Formula/doit.rb
@@ -3,7 +3,7 @@ class Notes < Formula
   homepage "https://github.com/RichieMobile/doit"
   url "https://github.com/RichieMobile/doit/archive/1.0.0.tar.gz"
   version "1.0.0"
-  sha256 "e17405adc655824dec3ca6e2a9a4b199a715743ed5f0948df58f6bb369267aa3" 
+  sha256 "" 
 
   def install
     bin.install "doit"
