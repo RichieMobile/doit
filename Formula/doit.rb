b5aba2fa81c850b6e05742313cfc8bf2da0a6824
William Crump <rickycrump95@gmail.com>
2018-10-31 14:07:51 -0400

    Added comment about how to update sha value.

--- Formula/doit.rb
+++ Formula/doit.rb
@@ -3,9 +3,12 @@ class Notes < Formula
   homepage "https://github.com/RichieMobile/doit"
   url "https://github.com/RichieMobile/doit/archive/1.0.0.tar.gz"
   version "1.0.0"
-  sha256 "e17405adc655824dec3ca6e2a9a4b199a715743ed5f0948df58f6bb369267aa3"
+  sha256 "e17405adc655824dec3ca6e2a9a4b199a715743ed5f0948df58f6bb369267aa3" 
 
   def install
     bin.install "doit"
   end
 end
+
+# to get a new sha256 value, create the release on github, download the source tar.gz
+# run `shasum -a 256 <tar.gz>`, then update the sha256 value above
