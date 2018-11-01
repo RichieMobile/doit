7ee9356b60abfa1e9a4f6aff0852c9ad46f1b61e
William Crump <rickycrump95@gmail.com>
2018-11-01 07:36:00 -0400

    Updated shasum and url for homebrew.

--- Formula/doit.rb
+++ Formula/doit.rb
@@ -1,9 +1,9 @@
 class Doit < Formula
   desc "Allows for creating, viewing, editing tasks from command line."
   homepage "https://github.com/RichieMobile/doit"
-  url "https://github.com/RichieMobile/doit/archive/1.0.0.tar.gz"
+  url "https://github.com/RichieMobile/doit/archive/v1.0.0.tar.gz"
   version "1.0.0"
-  sha256 "" 
+  sha256 "3908d947813062b5e182875d60e3b017e314bc5f4fc5136bd1aba801621160a9" 
 
   def install
     bin.install "doit"
