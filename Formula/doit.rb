2b28f4e70e1c912bf01f54bb6a95ec241ea417ee
RichieMobile <ricky@rickymbp.strsoftware.com>
2018-11-14 07:54:15 -0500

    Changed archive link for homebrew.

--- Formula/doit.rb
+++ Formula/doit.rb
@@ -1,7 +1,7 @@
 class Doit < Formula
   desc "Allows for creating, viewing, and completing tasks from command line."
   homepage "https://github.com/RichieMobile/doit"
-  url "https://github.com/RichieMobile/doit/archive/v1.0.0.tar.gz"
+  url "https://github.com/RichieMobile/doit/archive/v2.0.0.tar.gz"
   version "2.0.0"
   sha256 "f9cad467845047db8e2277dc097dcc4ec4d1ae282f7e2d6e976f408cb4df2569" 
 
