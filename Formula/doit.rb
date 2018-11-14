71877519e802742b1c4b25ff0a606f93646d4bb0
RichieMobile <ricky@rickymbp.strsoftware.com>
2018-11-14 07:51:30 -0500

    updated version and sha for homebrew.

--- Formula/doit.rb
+++ Formula/doit.rb
@@ -2,8 +2,8 @@ class Doit < Formula
   desc "Allows for creating, viewing, and completing tasks from command line."
   homepage "https://github.com/RichieMobile/doit"
   url "https://github.com/RichieMobile/doit/archive/v1.0.0.tar.gz"
-  version "1.0.0"
-  sha256 "3908d947813062b5e182875d60e3b017e314bc5f4fc5136bd1aba801621160a9" 
+  version "2.0.0"
+  sha256 "f9cad467845047db8e2277dc097dcc4ec4d1ae282f7e2d6e976f408cb4df2569" 
 
   def install
     bin.install "doit"
